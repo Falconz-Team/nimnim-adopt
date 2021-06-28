7f75c837ee66119cce82f57c4da6b772283176dd
rawan ahmad fawzi khasawneh <rawanahmed115@gmail.com>
2021-06-28 22:58:54 +0300

    modal page

--- src/components/Cats.js
+++ src/components/Cats.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap';
 import axios from 'axios';
+import './Cats.css';
+import SelectedPets from './SelectedPets';
+
 
 export class Cats extends Component {
   constructor(props) {
@@ -9,15 +12,49 @@ export class Cats extends Component {
       serverUrl: process.env.REACT_APP_SERVER_URL,
       image_Url: '',
       petData: [],
+      breed: '',
+      description: '',
+      Show: false,
+      Breed:'',
+      Age:'',
+      selected:'',
+      setShow: false,
 
     };
+    this.hideModal = this.hideModal.bind(this);
   }
+  hideModal = () => {
+    this.setState({ setShow: false });
+  };
+  
+
+  ClickNow = (breedData) => {
+    let selected = 0;
+    this.state.petData.map(value => {
+      if (value.breed === breedData) {
+        selected = value;
+      }
+      return selected ;
+    })
+    this.setState({
+      selected: selected,
+      Show: true,
+    })
+  }
+  handleClose = ()=>{
+    this.setState({
+      setShow: false,
+    })
+  }
+
 
   componentDidMount = () => {
     axios.get(`${this.state.serverUrl}/pets`).then((response, idx) => {
 
       this.setState({
         image_Url: this.state.image_Url,
+        description: this.state.description,
+        breed: this.state.breed,
         petData: response.data.cats,
 
       });
@@ -30,19 +67,39 @@ export class Cats extends Component {
   }
   render() {
     return (
-
-      <Container>
+      <>
         <Row xs={1} md={3} className="g-4">
           {this.state.petData.map((obj, idx) => {
             return (
-              <Image to = "/AddPets" src={obj.image_Url} thumbnail />
+              <>
+                <div class="cat">
+                  <img src={obj.image_Url} alt="Avatar" class="image" />
+                  <div class="overlay">
+                    <div class="text">{obj.breed}
+                      <br></br><Button onClick={() => {
+                        this.ClickNow(obj.breed);
+                        
+                      }}>More Information</Button>{' '}
+                    </div>
+                  </div>
+                </div>
+              </>
             );
           })}
-        </Row>
-      </Container>
-
+          <SelectedPets
+                  breed={this.state.selected.breed}
+                  Age={this.state.selected.age}
+                  gender={this.state.selected.gender}
+                  description={this.state.selected.description}
+                  Image_Url={this.state.selected.image_Url}
+                  shows={this.state.Show}
+                  HideModal={this.hideModal}
+                />
+          
+          {console.log(this.state.Show)}
+          </Row>
+      </>
     );
   }
 }
-
 export default Cats;
