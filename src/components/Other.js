b33996fdb583cae2764538c95c8e04cab1f1942e
shahdaljalam <shahd.jm33@gmail.com>
2021-06-29 20:46:54 +0300

    fixed the model2

--- src/components/Other.js
+++ src/components/Other.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap';
 import axios from 'axios';
+import './ShowPets.css';
+import SelectedPets from './SelectedPets';
 
 
 export class Other extends Component {
@@ -10,13 +12,40 @@ export class Other extends Component {
       serverUrl: process.env.REACT_APP_SERVER_URL,
       image_Url: '',
       petData: [],
+      breed: '',
+      description: '',
+      Show: false,
+      Breed:'',
+      Age:'',
+      selected:''
     };
   }
+  hideModal = () => {
+    this.setState({ Show: !this.state.Show });
+  };
+  
+
+  ClickNow = (breedData) => {
+    let selected = 0;
+    this.state.petData.map(value => {
+      if (value.breed === breedData) {
+        selected = value;
+      }
+      return selected ;
+    })
+    this.setState({
+      selected: selected,
+      Show: true,
+    })
+  }
+
   componentDidMount = () => {
     axios.get(`${this.state.serverUrl}/pets`).then((response, idx) => {
 
       this.setState({
         image_Url: this.state.image_Url,
+        description: this.state.description,
+        breed: this.state.breed,
         petData: response.data.other,
 
       });
@@ -29,17 +58,39 @@ export class Other extends Component {
   }
   render() {
     return (
-      <Container>
-        <Row xs={1} md={3} className="g-4">
+      <div class = 'modelpets'>
+        <Row xs={1} md={3} className="g-4" style={{marginTop:'80px'}}>
           {this.state.petData.map((obj, idx) => {
             return (
-              <Image to="/AddPets" src={obj.image_Url} thumbnail />
+              <>
+                <div class="cat" >
+                  <img src={obj.image_Url} alt="Avatar" class="image" style={{height:'460px'}} />
+                  <div class="overlay">
+                    <div class="text">{obj.breed}
+                      <br></br><Button onClick={() => {
+                        this.ClickNow(obj.breed);
+                        
+                      }}>More Information</Button>{' '}
+                    </div>
+                  </div>
+                </div>
+              </>
             );
           })}
-        </Row>
-      </Container>
+          <SelectedPets
+                  breed={this.state.selected.breed}
+                  Age={this.state.selected.age}
+                  gender={this.state.selected.gender}
+                  description={this.state.selected.description}
+                  Image_Url={this.state.selected.image_Url}
+                  Show={this.state.Show}
+                  hideModal={this.hideModal}
+                />
+          
+          {console.log(this.state.Show)}
+          </Row>
+      </div>
     );
   }
 }
-
 export default Other;
--- src/components/SelectedPets.js
+++ src/components/SelectedPets.js
@@ -22,25 +22,28 @@ export class SelectedPets extends Component {
             <div>
             {console.log('1')}
             <Modal style={{ background: 'rgba(52, 52, 52, 0.6)' }} size='xl' show={this.props.Show} >
-                <Modal.Header closeButton>
+                <Modal.Header >
                     <Modal.Title style={{ color: 'black' }}>{this.props.breed}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body style={{  display: 'grid', gridTemplateColumns: '50% 50%' }}>
-                    <Card style={{height: '490px'}}>
-                        <img src={this.props.Image_Url} alt="image" />
+                    <Card >
+                        <img src={this.props.Image_Url} alt="image" style={{height: '460px'}}s />
                     </Card>
-                    <div style={{marginLeft: '30px' }}>
+                    <div style={{marginLeft: '30px' , marginTop:'50px'}}>
                         <div>
                             <h4>Gender: {this.props.gender}</h4>
                             <h4>Age: {this.props.Age}</h4>
+                            <br></br>
                         </div>
-                        <div style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '5px' }}>
+                        <div style={{ backgroundColor: 'rgba(280, 233, 300, 0.999)', textAlign: 'center', padding: '5px' }}>
                             <p><strong>{this.props.description}</strong></p>
+                        </div>
+                        <div style={{marginTop:'20px' , marginLeft:'200px'}}>
+                        <Button>Adopt Now</Button>
                         </div>
                     </div>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button>Adopt Now</Button>
                     {console.log(this.props.HideModal)}
                     <Button variant="secondary" onClick={this.props.hideModal}>Close</Button>
                 </Modal.Footer>
