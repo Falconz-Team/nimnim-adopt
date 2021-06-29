924a6d2c838987f6f5a8d7dd5e31b3383db283f1
shahdaljalam <shahd.jm33@gmail.com>
2021-06-29 17:22:49 +0300

    fixed the model

--- src/components/SelectedPets.js
+++ src/components/SelectedPets.js
@@ -20,32 +20,32 @@ export class SelectedPets extends Component {
     render() {
         return (
             <div>
-                {console.log('1')}
-                <Modal size = 'xl'show={this.props.shows} >
-                    <Modal.Header closeButton>
-                        <Modal.Title style={{ color: 'black' }}>{this.props.breed}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Card style={{ backgroundColor: 'black' }}>
-                            <img src={this.props.Image_Url} alt="image" />
-                        </Card>
-                    </Modal.Body>
-                    <Modal.Body>
-                        {this.props.gender}
-                    </Modal.Body>
-                    <Modal.Body>
-                        {this.props.Age}
-                    </Modal.Body>
-                    <Modal.Body>
-                        {this.props.description}
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button>Adopt Now</Button>
-                        {console.log(this.props.HideModal)}
-                        <Button onClick={this.HideModal}>Close</Button>
-                    </Modal.Footer>
-                </Modal>
-            </div>
+            {console.log('1')}
+            <Modal style={{ background: 'rgba(52, 52, 52, 0.6)' }} size='xl' show={this.props.Show} >
+                <Modal.Header closeButton>
+                    <Modal.Title style={{ color: 'black' }}>{this.props.breed}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{  display: 'grid', gridTemplateColumns: '50% 50%' }}>
+                    <Card style={{height: '490px'}}>
+                        <img src={this.props.Image_Url} alt="image" />
+                    </Card>
+                    <div style={{marginLeft: '30px' }}>
+                        <div>
+                            <h4>Gender: {this.props.gender}</h4>
+                            <h4>Age: {this.props.Age}</h4>
+                        </div>
+                        <div style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '5px' }}>
+                            <p><strong>{this.props.description}</strong></p>
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button>Adopt Now</Button>
+                    {console.log(this.props.HideModal)}
+                    <Button variant="secondary" onClick={this.props.hideModal}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
         );
     }
 }
