f6f241cd57c6e3ec9ac8107243d20701fc42171d
Samer Odeh <samerodeh220@gmail.com>
2021-06-29 21:28:58 +0300

    CarePage & CategoryPage edits

--- src/components/ShowCategory.js
+++ src/components/ShowCategory.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Show-catogety.css';
 import {Card, Button} from 'react-bootstrap';
+import Cats from './Cats';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export class ShowCategory extends Component {
   render() {
@@ -19,15 +26,18 @@ export class ShowCategory extends Component {
 
             <br></br>
 
-            <p>Adoption is owning a pet and taking responsibility for it by adopting it from an animal shelter or from its old owner or by saving it yourself instead of buying it.
+            <p>Adoption is owning a pet and taking responsibility for it by adopting it from an animal  <br></br> shelter or from its old owner or by saving it yourself instead of buying it.
               <br></br>
-              The animals available for adoption are no less beautiful and energyless than the animals offered for sale.
               <br></br>
-              And perhaps the animals available for adoption will be in better health since there is someone who takes care of them.
+              The animals available for adoption are no less beautiful and energyless <br></br> than the animals offered for sale.
+              <br></br>
+              <br></br>
+              And perhaps the animals available for adoption will be in better health <br></br> since there is someone who takes care of them.
+              <br></br>
               <br></br>
               Many animal rescuers are also more grateful to their adopters.</p>
 
-            <br></br>
+            {/* <br></br> */}
           </div>
         </div>
 
@@ -36,10 +46,73 @@ export class ShowCategory extends Component {
           <div className='container'>
             <div className='row row-cols-6'>
               <div className='col mb-5'>
-                <Button variant="info"href="/CarePage">CarePage</Button>
+                {/* <Button class="ToLearn" variant="info"href="/CarePage">To learn more </Button> */}
+                <Link to="/CarePage">
+                    <button class="ToLearn" type="button">To learn more</button>
+                    </Link>
               </div>
             </div>
-            <div className='row row-cols-4 '>
+
+            <>
+        <div class="Pets" >
+          <h2>Pets Category</h2>
+        </div>
+        <br></br>
+                <div class="AnimalsCategory">
+                
+                  <img src="https://images.unsplash.com/photo-1596710104472-32ce1fb38f8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" alt="Avatar" class="image1" />
+                  
+                  <div class="overlay1">
+                    <div class="text1">{}
+                    <Link to="/Cats">
+                    <button class="button" type="button">Show Cats</button>
+                    </Link>
+                    </div>
+                  </div>
+                </div>
+              </>
+
+              <>
+                <div class="AnimalsCategory">
+                  <img src="https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Avatar" class="image1" />
+                  <div class="overlay1">
+                    <div class="text1">{}
+                    <Link to="/Dogs">
+                    <button class="button" type="button">Show Dogs</button>
+                    </Link>
+                    </div>
+                  </div>
+                </div>
+              </>
+
+              <>
+                <div class="AnimalsCategory">
+                  <img src="https://images.unsplash.com/photo-1480044965905-02098d419e96?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="Avatar" class="image1" />
+                  <div class="overlay1">
+                    <div class="text1">{}
+                    <Link to="/Birds">
+                    <button class="button" type="button">Show Birds</button>
+                    </Link>
+                    </div>
+                  </div>
+                </div>
+              </>
+
+              <>
+                <div class="AnimalsCategory">
+                  <img src="https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80" alt="Avatar" class="image1" />
+                  <div class="overlay1">
+                    <div class="text1">{}
+                    <Link to="/Other">
+                    <button class="button" type="button">Show Other</button>
+                    </Link>
+                    </div>
+                  </div>
+                </div>
+              </>
+              <br></br>
+
+      {/*       <div className='row row-cols-4 '>
               <div className='col'><Card style={{ width: '15rem' }}>
                 <Card.Img variant="top" src="https://images.unsplash.com/photo-1596710104472-32ce1fb38f8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" />
                 <Card.Body>
@@ -67,11 +140,11 @@ export class ShowCategory extends Component {
                  
                   <Button variant="info" href="/Other">OTHER</Button>
                 </Card.Body>
-              </Card></div>
+              </Card></div> */}
             </div>
           </div>
         </div>
-      </div>
+      /* </div> */
     );
   }
 }
